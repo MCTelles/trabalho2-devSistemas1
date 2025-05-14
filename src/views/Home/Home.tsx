04633ab4198d1703e5329ad83a0223495ecf1376
MCTelles <marcelotellesest@gmail.com>
2025-05-14 14:58:57 -0300

    finishing the first screen

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,15 +1,32 @@
 import styles from "./Home.module.scss";
 
 import logoShopee from "../../assets/logoShopeeAMF.png";
+import arrowIcon from "../../assets/Arrow.svg";
 
 function Home() {
   return (
     <div className={styles.homeContainer}>
-      <div className={styles.circulo}>
+      <div className={styles.circuloLogo}>
         <img src={logoShopee} alt="" />
       </div>
+
       <h1>ShopeeAMF</h1>
-      <span>Beautiful eCommerce UI Kit for your online store</span>
+
+      <span className={styles.greetingsSpan}>
+        Beautiful eCommerce UI Kit for your online store
+      </span>
+
+      <button className={styles.createAccount}>Let's get started</button>
+
+      <div className={styles.loginHomeContainer}>
+        <span className={styles.accountCreateSpan}>
+          I already have an account
+        </span>
+
+        <button className={styles.loginButton}>
+          <img className={styles.arrow} src={arrowIcon} alt="Setinha" />
+        </button>
+      </div>
     </div>
   );
 }
