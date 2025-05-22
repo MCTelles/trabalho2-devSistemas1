fcc6558fb0c0b9aa4d51641ad7fabeb3133e49d8
MCTelles <marcelotellesest@gmail.com>
2025-05-22 16:06:05 -0300

    starting login page

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-
 import styles from "./Login.module.scss";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className={styles.loginContainer}>
+      <h1>Login</h1>
+
+      <span>Good to see you back!</span>
+
+      <input type="email" placeholder="Email" />
+
+      <button className={styles.loginDoneButton}>Login</button>
+
+      <button className={styles.loginCancelButton}>Cancel</button>
+    </div>
+  );
 }
 
 export { Login };
